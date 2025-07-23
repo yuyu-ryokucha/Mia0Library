3a8a5915d4a8c06f5ed25a2e37ec4ae4e55c92d2
YUYU <110108254+yuyu-ryokucha@users.noreply.github.com>
2025-07-23 12:08:07 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Mia0Library",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Mia0Library",
+            targets: ["Mia0Library"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Mia0Library"),
+        .testTarget(
+            name: "Mia0LibraryTests",
+            dependencies: ["Mia0Library"]
+        ),
+    ]
+)
